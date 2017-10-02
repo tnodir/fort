6cdc4cdc8565041ac699a5e6485cf393a1dd1540
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2017-10-02 08:33:49 +0500

    Driver: Improve scripts.

---
+++ src/driver/scripts/testsigning-off.bat
@@ -0,0 +1,3 @@
+BcdEdit.exe -set loadoptions ENABLE_INTEGRITY_CHECKS
+BcdEdit.exe -set NOINTEGRITYCHECKS OFF
+BcdEdit.exe -set TESTSIGNING OFF
--- src/driver/scripts/testsigning-on.bat
+++ src/driver/scripts/testsigning-on.bat
@@ -1,1 +1,3 @@
+BcdEdit.exe -set loadoptions DISABLE_INTEGRITY_CHECKS
+BcdEdit.exe -set NOINTEGRITYCHECKS ON
 BcdEdit.exe -set TESTSIGNING ON
