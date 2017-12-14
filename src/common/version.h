3e23cc8342f64368d6e2ca15938f7650bb1f8d83
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2017-12-14 19:05:21 +0500

    Bump version.

--- src/common/version.h
+++ src/common/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APP_NAME		"Fort Firewall"
-#define APP_VERSION_STR		"1.3.1"
-#define APP_VERSION		0x010301
+#define APP_VERSION_STR		"1.4.0"
+#define APP_VERSION		0x010400
 #define APP_UPDATES_URL		"https://github.com/tnodir/fort/releases"
 
 #define FORT_DRIVER_VERSION	3
