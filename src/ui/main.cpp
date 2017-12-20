f6a1aa6d6378f0483355754588f949b738bc3c26
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2017-12-20 19:15:48 +0500

    UI: Enable high dpi pixmaps.

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -9,7 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
     QApplication app(argc, argv);
     app.setApplicationName(APP_NAME);
