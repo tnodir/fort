6ab65b08074b6a5ef84fefc2b636a84483b861e1
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2018-01-14 10:33:14 +0500

    Hotfix version.

--- src/common/version.h
+++ src/common/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APP_NAME		"Fort Firewall"
-#define APP_VERSION_STR		"1.7.0"
-#define APP_VERSION		0x010700
+#define APP_VERSION_STR		"1.6.1"
+#define APP_VERSION		0x010601
 #define APP_UPDATES_URL		"https://github.com/tnodir/fort/releases"
 #define APP_UPDATES_API_URL	"https://api.github.com/repos/tnodir/fort/releases/latest"
 
