fd6cdadd9c1f8dd7372e94d7866033c84fcabbf3
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2019-01-04 15:51:42 +0500

    Bump version.

--- src/common/version.h
+++ src/common/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APP_NAME		"Fort Firewall"
-#define APP_VERSION_STR		"2.1.0"
-#define APP_VERSION		0x020100
+#define APP_VERSION_STR		"2.2.0"
+#define APP_VERSION		0x020200
 #define APP_UPDATES_URL		"https://github.com/tnodir/fort/releases"
 #define APP_UPDATES_API_URL	"https://api.github.com/repos/tnodir/fort/releases/latest"
 
