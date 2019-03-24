d11bf43fd7df4c1198df6ee28c998e687f457092
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2019-03-24 10:28:49 +0500

    UI: Improve .rc

--- src/common/version.h
+++ src/common/version.h
@@ -1,9 +1,13 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define APP_NAME		"Fort Firewall"
+#define APP_VERSION_MAJOR	2
+#define APP_VERSION_MINOR	6
+#define APP_VERSION_PATCH	0
 #define APP_VERSION_STR		"2.6.0"
 #define APP_VERSION		0x020600
+
+#define APP_NAME		"Fort Firewall"
 #define APP_UPDATES_URL		"https://github.com/tnodir/fort/releases"
 #define APP_UPDATES_API_URL	"https://api.github.com/repos/tnodir/fort/releases/latest"
 
