6cb4f7f5d3f68e149c8ef9b669694902c2ba315f
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2019-03-26 11:52:28 +0500

    UI: Add AA_DisableWindowContextHelpButton attr.

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
 {
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QApplication::setAttribute(Qt::AA_DisableWindowContextHelpButton);
 
     QApplication::setQuitOnLastWindowClosed(false);
 
