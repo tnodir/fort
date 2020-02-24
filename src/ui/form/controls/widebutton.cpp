0f66fadac1fe3868a4274bcd078e7c7d57186711
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2020-02-24 14:43:02 +0500

    UI: Minor fix.

--- src/ui/form/controls/widebutton.cpp
+++ src/ui/form/controls/widebutton.cpp
@@ -13,6 +13,6 @@ WideButton::WideButton(const QIcon &icon, const QString &text, QWidget *parent) 
 QSize WideButton::minimumSizeHint() const
 {
     auto size = QPushButton::minimumSizeHint();
-    size.setWidth(size.width() + 6);
+    size.setWidth(size.width() + 3);
     return size;
 }
