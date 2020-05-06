c1672af1e21f8c00dfb5d417ba0f738126960701
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2020-05-06 19:16:03 +0300

    Tests: Fix FileUtil test.

--- src/tests/fileutil/test.cpp
+++ src/tests/fileutil/test.cpp
@@ -7,7 +7,12 @@
 void Test::paths()
 {
     const QString driveC("C:");
-    const QString kernelNameC("\\Device\\HarddiskVolume1");
+
+    const QString kernelNameC = FileUtil::driveToKernelName(driveC);
+    QVERIFY(kernelNameC.startsWith("\\Device\\HarddiskVolume", Qt::CaseSensitive));
+
+    QCOMPARE(FileUtil::kernelNameToDrive(kernelNameC), driveC);
+
     const QString subPathBack("\\test\\");
     const QString subPath = QString(subPathBack)
             .replace(QLatin1Char('\\'), QLatin1Char('/'));
@@ -15,9 +20,6 @@ void Test::paths()
     const QString kernelPath = kernelNameC + subPathBack;
     const QString pathBack = driveC + subPathBack;
     const QString path = driveC + subPath;
-
-    QCOMPARE(FileUtil::kernelNameToDrive(kernelNameC), driveC);
-    QCOMPARE(FileUtil::driveToKernelName(driveC), kernelNameC);
 
     QCOMPARE(FileUtil::kernelPathToPath(kernelPath), pathBack);
     QCOMPARE(FileUtil::pathToKernelPath(path, false), kernelPath);
