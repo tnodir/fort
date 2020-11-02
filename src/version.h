8ac85f39583feffc8c2a88d7d026afed227225bd
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2020-11-02 14:25:10 +0300

    Driver: Bump version.

--- src/version.h
+++ src/version.h
@@ -14,6 +14,6 @@
 #define APP_UPDATES_URL		"https://github.com/tnodir/fort/releases"
 #define APP_UPDATES_API_URL	"https://api.github.com/repos/tnodir/fort/releases/latest"
 
-#define DRIVER_VERSION		21
+#define DRIVER_VERSION		22
 
 #endif // VERSION_H
