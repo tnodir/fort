3fda35fdbc90f7e38b2f15cfef79ae925a2fd07c
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2020-12-20 15:22:34 +0300

    Deploy: Fix qt-build.bat to use CMake with Qt 6.0

--- deploy/qt-build.bat
+++ deploy/qt-build.bat
@@ -5,25 +5,27 @@
 @cd %~dp0
 @echo off
 
+@set QT_PATH=../../../qt
 @set TARGET_PATH=build-qt
 
 md %TARGET_PATH%
 cd %TARGET_PATH%
 
-../../../qt/configure -release -optimize-size -qmake -opensource -confirm-license -no-opengl ^
-	-nomake examples -nomake tests -nomake tools -mp -ltcg ^
-	-skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d ^
-	-skip qtcharts -skip qtconnectivity -skip qtdatavis3d ^
-	-skip qtdoc -skip qtdeclarative -skip qtdocgallery ^
+%QT_PATH%/configure -release -optimize-size -opensource -confirm-license ^
+	-mp -no-opengl -nomake examples -nomake tests -nomake tools ^
+	^
+	-skip qt3d -skip qt5compat -skip qtactiveqt -skip qtandroidextras ^
+	-skip qtcanvas3d -skip qtcharts -skip qtconnectivity -skip qtdatavis3d ^
+	-skip qtdeclarative -skip qtdoc -skip qtdocgallery ^
 	-skip qtfeedback -skip qtgamepad -skip qtgraphicaleffects ^
-	-skip qtlocation -skip qtmacextras -skip qtnetworkauth ^
-	-skip qtmultimedia -skip qtpim -skip qtpurchasing -skip qtqa ^
-	-skip qtquickcontrols -skip qtquickcontrols2 ^
-	-skip qtremoteobjects -skip qtrepotools ^
-	-skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech ^
-	-skip qtsvg -skip qtsystems -skip qttools -skip qtvirtualkeyboard ^
-	-skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebglplugin ^
-	-skip qtwebsockets -skip qtwebview ^
+	-skip qtlocation -skip qtlottie -skip qtmacextras -skip qtmultimedia ^
+	-skip qtnetworkauth -skip qtpim -skip qtpurchasing -skip qtqa ^
+	-skip qtquick3d -skip qtquickcontrols -skip qtquickcontrols2 ^
+	-skip qtquicktimeline -skip qtremoteobjects -skip qtrepotools -skip qtscxml ^
+	-skip qtsensors -skip qtserialbus -skip qtserialport -skip qtshadertools ^
+	-skip qtspeech -skip qtsvg -skip qtsystems -skip qttools -skip qttranslations ^
+	-skip qtvirtualkeyboard -skip qtwayland -skip qtwebchannel -skip qtwebengine ^
+	-skip qtwebglplugin -skip qtwebsockets -skip qtwebview ^
 	-skip qtwinextras -skip qtx11extras -skip qtxmlpatterns ^
 	^
 	-no-feature-columnview -no-feature-commandlinkbutton ^
@@ -36,7 +38,8 @@ cd %TARGET_PATH%
 	-no-feature-islamiccivilcalendar -no-feature-jalalicalendar ^
 	-no-feature-itemmodeltester -no-feature-lcdnumber -no-feature-listwidget ^
 	-no-feature-mdiarea -no-feature-movie -no-feature-pdf -no-feature-picture ^
-	-no-feature-printer -no-feature-printpreviewdialog -no-feature-printpreviewwidget ^
+	-no-feature-printer -no-feature-printdialog ^
+	-no-feature-printpreviewdialog -no-feature-printpreviewwidget ^
 	-no-feature-raster-64bit -no-feature-splashscreen ^
 	-no-feature-sqlmodel -no-feature-textbrowser -no-feature-textodfwriter ^
 	-no-feature-undocommand -no-feature-undogroup -no-feature-undostack -no-feature-undoview ^
@@ -45,9 +48,8 @@ cd %TARGET_PATH%
 	-no-feature-style-android -no-feature-style-mac -no-feature-style-windowsvista ^
 	^
 	-no-feature-network -no-feature-sql -no-feature-sql-odbc ^
-	-no-feature-xml -no-feature-xmlstream ^
 	^
-	-no-feature-gif -no-feature-jpeg -no-libjpeg -no-feature-tiff -no-feature-webp ^
+	-no-feature-gif -no-feature-tiff -no-feature-webp ^
 	-no-feature-dbus -no-feature-vulkan -no-feature-vkgen
 
-nmake
+cmake --build . --parallel
