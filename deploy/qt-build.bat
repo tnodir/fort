64795f01b6353f8f9f203ab01e8a477ff5602ef7
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2021-02-09 16:06:42 +0500

    Deploy: Fix qt-build.bat for Qt 6.0

--- deploy/qt-build.bat
+++ deploy/qt-build.bat
@@ -16,11 +16,11 @@ cd %TARGET_PATH%
 	^
 	-skip qt3d -skip qt5compat -skip qtactiveqt -skip qtandroidextras ^
 	-skip qtcanvas3d -skip qtcharts -skip qtconnectivity -skip qtdatavis3d ^
-	-skip qtdeclarative -skip qtdoc -skip qtdocgallery ^
-	-skip qtfeedback -skip qtgamepad -skip qtgraphicaleffects ^
-	-skip qtlocation -skip qtlottie -skip qtmacextras -skip qtmultimedia ^
-	-skip qtnetworkauth -skip qtpim -skip qtpurchasing -skip qtqa ^
-	-skip qtquick3d -skip qtquickcontrols -skip qtquickcontrols2 ^
+	-skip qtdeclarative -skip qtdoc -skip qtfeedback ^
+	-skip qtgamepad -skip qtgraphicaleffects -skip qtimageformats ^
+	-skip qtlocation -skip qtlottie -skip qtmacextras ^
+	-skip qtmultimedia -skip qtnetworkauth -skip qtpim -skip qtpurchasing ^
+	-skip qtqa -skip qtquick3d -skip qtquickcontrols -skip qtquickcontrols2 ^
 	-skip qtquicktimeline -skip qtremoteobjects -skip qtrepotools -skip qtscxml ^
 	-skip qtsensors -skip qtserialbus -skip qtserialport -skip qtshadertools ^
 	-skip qtspeech -skip qtsvg -skip qtsystems -skip qttools -skip qttranslations ^
@@ -30,26 +30,22 @@ cd %TARGET_PATH%
 	^
 	-no-feature-columnview -no-feature-commandlinkbutton ^
 	-no-feature-concatenatetablesproxymodel ^
-	-no-feature-concurrent -no-feature-cups -no-feature-datawidgetmapper ^
-	-no-feature-dial -no-feature-dockwidget ^
-	-no-feature-filesystemwatcher -no-feature-fontcombobox ^
+	-no-feature-concurrent -no-feature-datawidgetmapper -no-feature-dial ^
+	-no-feature-dockwidget -no-feature-filesystemwatcher -no-feature-fontcombobox ^
 	-no-feature-fontdialog -no-feature-hijricalendar -no-feature-identityproxymodel ^
-	-no-feature-imageformat_jpeg -no-feature-imageformat_ppm ^
 	-no-feature-islamiccivilcalendar -no-feature-jalalicalendar ^
 	-no-feature-itemmodeltester -no-feature-lcdnumber -no-feature-listwidget ^
 	-no-feature-mdiarea -no-feature-movie -no-feature-pdf -no-feature-picture ^
-	-no-feature-printer -no-feature-printdialog ^
-	-no-feature-printpreviewdialog -no-feature-printpreviewwidget ^
-	-no-feature-raster-64bit -no-feature-splashscreen ^
+	-no-feature-printsupport -no-feature-raster-64bit -no-feature-splashscreen ^
 	-no-feature-sqlmodel -no-feature-textbrowser -no-feature-textodfwriter ^
 	-no-feature-undocommand -no-feature-undogroup -no-feature-undostack -no-feature-undoview ^
 	-no-feature-whatsthis -no-feature-wizard ^
 	^
 	-no-feature-style-android -no-feature-style-mac -no-feature-style-windowsvista ^
 	^
-	-no-feature-network -no-feature-sql -no-feature-sql-odbc ^
+	-no-feature-mimetype-database -no-feature-network -no-feature-sql -no-feature-sql-odbc ^
 	^
-	-no-feature-gif -no-feature-tiff -no-feature-webp ^
+	-no-feature-gif -no-feature-jpeg ^
 	-no-feature-dbus -no-feature-vulkan -no-feature-vkgen
 
 cmake --build . --parallel
