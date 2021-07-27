cf13cdf6e9b7c6f4e813868e0ed102b4ffeb1589
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2021-07-27 15:31:59 +0300

    UI: Util/IconCache: Check non-main thead usage.

--- src/ui/util/iconcache.cpp
+++ src/ui/util/iconcache.cpp
@@ -1,25 +1,51 @@
 #include "iconcache.h"
 
+#include <QDebug>
 #include <QIcon>
 #include <QPixmapCache>
+#include <QThread>
+
+namespace {
+
+void checkThread()
+{
+#ifndef QT_NO_DEBUG
+    static const Qt::HANDLE g_mainThreadId = QThread::currentThreadId();
+
+    const Qt::HANDLE threadId = QThread::currentThreadId();
+    if (g_mainThreadId != threadId) {
+        qWarning() << "QPixmap used by non-main thread:" << threadId << "; main:" << g_mainThreadId;
+    }
+#endif
+}
+
+}
 
 bool IconCache::find(const QString &key, QPixmap *pixmap)
 {
+    checkThread();
+
     return QPixmapCache::find(key, pixmap);
 }
 
 bool IconCache::insert(const QString &key, const QPixmap &pixmap)
 {
+    checkThread();
+
     return QPixmapCache::insert(key, pixmap);
 }
 
 void IconCache::remove(const QString &key)
 {
+    checkThread();
+
     return QPixmapCache::remove(key);
 }
 
 QPixmap IconCache::file(const QString &filePath)
 {
+    checkThread();
+
     QPixmap pixmap;
     if (!find(filePath, &pixmap)) {
         pixmap.load(filePath);
@@ -30,5 +56,7 @@ QPixmap IconCache::file(const QString &filePath)
 
 QIcon IconCache::icon(const QString &filePath)
 {
+    checkThread();
+
     return filePath.isEmpty() ? QIcon() : QIcon(file(filePath));
 }
