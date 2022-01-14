tnodir/fort

commit d8889865459fd89f22fb1de6638b5742b63b3ff2

UI: LogManager: Continue to read log only on success

src/ui/log/logmanager.cpp
@@ -100,11 +100,11 @@ void LogManager::addFreeBuffer(LogBuffer *logBuffer)
 
 void LogManager::processLogBuffer(LogBuffer *logBuffer, bool success, quint32 errorCode)
 {
-    if (m_active) {
-        readLogAsync();
-    }
-
     if (success) {
+        if (m_active) {
+            readLogAsync();
+        }
+
         readLogEntries(logBuffer);
     } else {
         const auto errorMessage = OsUtil::errorMessage(errorCode);

commit e4a5ee079d1b3ac74708bd9168537b473d5961af

UI: LogManager: Fix log reading (Regression from d88898654)

src/ui/log/logmanager.cpp
@@ -105,13 +105,13 @@ void LogManager::addFreeBuffer(LogBuffer *logBuffer)
 
 void LogManager::processLogBuffer(LogBuffer *logBuffer, bool success, quint32 errorCode)
 {
-    if (success) {
-        if (m_active) {
-            readLogAsync();
-        }
+    if (m_active && (success || errorCode == 0)) {
+        readLogAsync();
+    }
 
+    if (success) {
         readLogEntries(logBuffer);
-    } else {
+    } else if (errorCode != 0) {
         const auto errorMessage = OsUtil::errorMessage(errorCode);
         setErrorMessage(errorMessage);
     }