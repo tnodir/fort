c24007676380c4e79b014ef178fd4f88e87834a5
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2023-01-19 18:49:54 +0300

    Deploy/Sign: Use Digicert timestamp server

--- deploy/sign/sign-env-certum.bat
+++ deploy/sign/sign-env-certum.bat
@@ -4,4 +4,4 @@
 @set CRT_PATH=cert\Certum Trusted Network CA.crt
 @set CRT_NAME=Open Source Developer, Nodir Temirkhodjaev
 
-@set TS_URL=http://timestamp.apple.com/ts01
+@set TS_URL=http://timestamp.digicert.com/
