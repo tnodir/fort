a57e0918b21af4cc812bc188ec2d0b860d68dafd
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2023-01-25 17:22:44 +0300

    UI: Bump version

--- src/version/fort_version.h
+++ src/version/fort_version.h
@@ -3,14 +3,14 @@
 
 #define APP_VERSION_MAJOR	3
 #define APP_VERSION_MINOR	6
-#define APP_VERSION_PATCH	8
-#define APP_VERSION_STR		"3.6.8"
-#define APP_VERSION		0x030608
+#define APP_VERSION_PATCH	9
+#define APP_VERSION_STR		"3.6.9"
+#define APP_VERSION		0x030609
 
 #define APP_NAME		"Fort Firewall"
 #define APP_BASE		"FortFirewall"
 #define APP_PUBLISHER		"Nodir Temirkhodjaev"
-#define APP_LEGALCOPYRIGHT	"Copyright (C) 2015-2022 Nodir Temirkhodjaev. All Rights Reserved."
+#define APP_LEGALCOPYRIGHT	"Copyright (C) 2015-2023 Nodir Temirkhodjaev. All Rights Reserved."
 #define APP_URL			"https://github.com/tnodir/fort"
 #define APP_UPDATES_URL		"https://github.com/tnodir/fort/releases"
 #define APP_UPDATES_API_URL	"https://api.github.com/repos/tnodir/fort/releases/latest"
