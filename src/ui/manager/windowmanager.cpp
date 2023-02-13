0a2c48b7ef4765871c9e4df1d2877904fd76f22a
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2023-02-13 10:39:59 +0300

    Revert "UI: WindowManager: Use colorSchemeChanged"
    The "colorScheme" is not functional yet.
    
    This reverts commit a0ec8b3d3e212096063e82e01add32af6ca42ee9.

--- src/ui/manager/windowmanager.cpp
+++ src/ui/manager/windowmanager.cpp
@@ -52,7 +52,7 @@ void WindowManager::setUp()
     setupMainWindow();
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
-    connect(QApplication::styleHints(), &QStyleHints::colorSchemeChanged, this,
+    connect(QApplication::styleHints(), &QStyleHints::appearanceChanged, this,
             &WindowManager::setupAppPalette);
 #endif
 
