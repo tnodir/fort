80f001cca0d055b3afb9686e6c7f6df597536ba4
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2023-04-03 20:18:01 +0300

    Driver: fortdrv: Fix typo

--- src/driver/fortdrv.c
+++ src/driver/fortdrv.c
@@ -73,6 +73,7 @@ static NTSTATUS fort_driver_load(PDRIVER_OBJECT driver, PUNICODE_STRING reg_path
     driver->MajorFunction[IRP_MJ_CLOSE] = &fort_device_close;
     driver->MajorFunction[IRP_MJ_CLEANUP] = &fort_device_cleanup;
     driver->MajorFunction[IRP_MJ_DEVICE_CONTROL] = &fort_device_control;
+    driver->MajorFunction[IRP_MJ_SHUTDOWN] = &fort_device_shutdown;
 
     return fort_device_load(device_obj);
 }
