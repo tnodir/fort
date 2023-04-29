a7af945b4628c25ce9b29624d66e679373b27cc6
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2023-04-29 09:59:25 +0300

    Driver: fortdbg: fort_check_stack: Tweak status code

--- src/driver/fortdbg.c
+++ src/driver/fortdbg.c
@@ -4,7 +4,8 @@
 
 #define FORT_KERNEL_STACK_MIN_SIZE 1024
 
-#define FORT_STATUS_INVALID_STACK 0x000000DC /* DRIVER_INVALID_STACK_ACCESS */
+#define FORT_STATUS_INVALID_STACK 0x0000007F /* UNEXPECTED_KERNEL_MODE_TRAP */
+#define FORT_TRAP_CODE            0x0000000C /* STACK_FAULT */
 
 FORT_API void fort_check_stack(const char *func_name, FORT_FUNC_ID func_id)
 {
@@ -15,5 +16,6 @@ FORT_API void fort_check_stack(const char *func_name, FORT_FUNC_ID func_id)
 
     LOG("Stack Overflow: %s: id=%d remaining=%d\n", func_name, func_id, free_size);
 
-    KeBugCheckEx(FORT_STATUS_INVALID_STACK, (FORT_DEBUG_VERSION << 16) | func_id, free_size, 0, 0);
+    KeBugCheckEx(FORT_STATUS_INVALID_STACK, FORT_TRAP_CODE, (FORT_DEBUG_VERSION << 16) | func_id,
+            free_size, 0);
 }
