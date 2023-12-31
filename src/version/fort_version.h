da8d70a0f431d2275040760ef1d447b889091a30
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2023-12-31 18:26:02 +0300

    UI: Bump version

--- src/version/fort_version.h
+++ src/version/fort_version.h
@@ -3,10 +3,10 @@
 
 #define APP_VERSION_MAJOR	3
 #define APP_VERSION_MINOR	10
-#define APP_VERSION_PATCH	5
-#define APP_VERSION_STR		"3.10.5"
+#define APP_VERSION_PATCH	6
+#define APP_VERSION_STR		"3.10.6"
 #define APP_VERSION_BUILD_STR	""
-#define APP_VERSION		0x031005
+#define APP_VERSION		0x031006
 
 #define APP_NAME		"Fort Firewall"
 #define APP_BASE		"FortFirewall"
