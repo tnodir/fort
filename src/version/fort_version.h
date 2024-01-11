1a877c6ff5de63674587d4945190d640e567c4c3
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2024-01-11 19:58:29 +0300

    UI: Bump build version

--- src/version/fort_version.h
+++ src/version/fort_version.h
@@ -5,7 +5,7 @@
 #define APP_VERSION_MINOR	10
 #define APP_VERSION_PATCH	8
 #define APP_VERSION_STR		"3.10.8"
-#define APP_VERSION_BUILD_STR	""
+#define APP_VERSION_BUILD_STR	"-test01"
 #define APP_VERSION		0x031008
 
 #define APP_NAME		"Fort Firewall"
