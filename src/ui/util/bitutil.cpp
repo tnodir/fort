a8ec214c724b02fbb39ca2b3e7329876e3d7f0de
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2024-02-20 18:33:08 +0300

    UI: BitUtil: Improve bitCount() for ARM64

--- src/ui/util/bitutil.cpp
+++ src/ui/util/bitutil.cpp
@@ -11,15 +11,27 @@ int BitUtil::firstZeroBit(quint32 u)
 
 int BitUtil::bitCount(quint32 u)
 {
-    /* // The following code is optimized to __popcnt by MSVC 17.9
+#if 0 // TODO: COMPAT: Enable after Win 11 24H2
+// #if (defined(_M_IX86) || defined(_M_X64)) && QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
+    return __popcnt(u);
+#elif defined(_M_ARM64)
+    __n64 num;
+    num.n64_u64[0] = u;
+
+    num = neon_cnt(num);
+    return num.n64_u32[0];
+#else
+    // From http://tekpool.wordpress.com/category/bit-count/
+    const quint32 uCount = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111);
+    return ((uCount + (uCount >> 3)) & 030707070707) % 63;
+
+    // The following code is optimized to __popcnt by MSVC 17.9
+    /*
     u = u - ((u >> 1) & 0x55555555);
     u = (u & 0x33333333) + ((u >> 2) & 0x33333333);
     return ((u + (u >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
     */
-
-    // From http://tekpool.wordpress.com/category/bit-count/
-    const quint32 uCount = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111);
-    return ((uCount + (uCount >> 3)) & 030707070707) % 63;
+#endif
 }
 
 int BitUtil::bitScanForward(quint32 mask)
