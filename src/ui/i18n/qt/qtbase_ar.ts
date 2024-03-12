28c1658a25924b91d7bd14211b928e8023700b3b
OthmanAliModaes <148161949+OthmanAliModaes@users.noreply.github.com>
2024-03-12 08:43:17 +0300

    Create Arabic translation file: qtbase_ar.ts
    Translation for QTBase.

---
+++ src/ui/i18n/qt/qtbase_ar.ts
@@ -0,0 +1,250 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar_SA">
+<context>
+    <name>CloseButton</name>
+    <message>
+        <source>Close Tab</source>
+        <translation>إ&amp;غلاق التبويب</translation>
+    </message>
+</context>
+<context>
+    <name>QAbstractSpinBox</name>
+    <message>
+        <source>&amp;Select All</source>
+        <translation>&amp;تحديد الكل</translation>
+    </message>
+    <message>
+        <source>&amp;Step up</source>
+        <translation>خطوة لأ&amp;على</translation>
+    </message>
+    <message>
+        <source>Step &amp;down</source>
+        <translation>خطوة لأ&amp;سفل</translation>
+    </message>
+</context>
+<context>
+    <name>QColorDialog</name>
+    <message>
+        <source>Hu&amp;e:</source>
+        <translation>در&amp;جة:</translation>
+    </message>
+    <message>
+        <source>&amp;Sat:</source>
+        <translation>ت&amp;شبع:</translation>
+    </message>
+    <message>
+        <source>&amp;Val:</source>
+        <translation>&amp;قيمة:</translation>
+    </message>
+    <message>
+        <source>&amp;Red:</source>
+        <translation>أ&amp;حمر:</translation>
+    </message>
+    <message>
+        <source>&amp;Green:</source>
+        <translation>أ&amp;خضر:</translation>
+    </message>
+    <message>
+        <source>Bl&amp;ue:</source>
+        <translation>أ&amp;زرق:</translation>
+    </message>
+    <message>
+        <source>A&amp;lpha channel:</source>
+        <translation>ال&amp;شفافية:</translation>
+    </message>
+    <message>
+        <source>&amp;HTML:</source>
+        <translation>&amp;HTML:</translation>
+    </message>
+    <message>
+        <source>Cursor at %1, %2
+Press ESC to cancel</source>
+        <translation>المؤشر عند %1, %2
+اضغط ESC للإلغاء</translation>
+    </message>
+    <message>
+        <source>&amp;Pick Screen Color</source>
+        <translation>القاط ل&amp;ون من الشاشة</translation>
+    </message>
+    <message>
+        <source>Cursor at %1, %2, color: %3
+Press ESC to cancel</source>
+        <translation>المؤشر عند %1, %2, اللون: %3
+اضغط ESC للإلغاء</translation>
+    </message>
+    <message>
+        <source>Select Color</source>
+        <translation>اختيار لون</translation>
+    </message>
+    <message>
+        <source>Pick Screen Color</source>
+        <translation>التقاط لون من الشاشة</translation>
+    </message>
+    <message>
+        <source>&amp;Basic colors</source>
+        <translation>الألوان الأ&amp;ساسية</translation>
+    </message>
+    <message>
+        <source>&amp;Custom colors</source>
+        <translation>الألوان ال&amp;مخصصة</translation>
+    </message>
+    <message>
+        <source>&amp;Add to Custom Colors</source>
+        <translation>إ&amp;ضافة إلى الألوان المخصصة</translation>
+    </message>
+</context>
+<context>
+    <name>QKeySequenceEdit</name>
+    <message>
+        <source>Press shortcut</source>
+        <translation>اضغط اختصارًا</translation>
+    </message>
+    <message>
+        <source>%1, ...</source>
+        <extracomment>This text is an &quot;unfinished&quot; shortcut, expands like &quot;Ctrl+A, ...&quot;</extracomment>
+        <translation>%1, ...</translation>
+    </message>
+</context>
+<context>
+    <name>QLineEdit</name>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>تراجع&amp;</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>إعا&amp;دة</translation>
+    </message>
+    <message>
+        <source>Cu&amp;t</source>
+        <translation>قص&amp;</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>نسخ&amp;</translation>
+    </message>
+    <message>
+        <source>&amp;Paste</source>
+        <translation>لصق&amp;</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>حذف&amp;</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation>تحديد الكل</translation>
+    </message>
+</context>
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <source>OK</source>
+        <translation>موافق</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>حفظ</translation>
+    </message>
+    <message>
+        <source>Save All</source>
+        <translation>حفظ الكل</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>فتح</translation>
+    </message>
+    <message>
+        <source>&amp;Yes</source>
+        <translation>نعم&amp;</translation>
+    </message>
+    <message>
+        <source>Yes to &amp;All</source>
+        <translation>نعم &amp;للكل</translation>
+    </message>
+    <message>
+        <source>&amp;No</source>
+        <translation>لا&amp;</translation>
+    </message>
+    <message>
+        <source>N&amp;o to All</source>
+        <translation>لا لل&amp;كل</translation>
+    </message>
+    <message>
+        <source>Abort</source>
+        <translation>إلغاء</translation>
+    </message>
+    <message>
+        <source>Retry</source>
+        <translation>إعادة المحاولة</translation>
+    </message>
+    <message>
+        <source>Ignore</source>
+        <translation>تجاهل</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>إغلاق</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>إلغاء</translation>
+    </message>
+    <message>
+        <source>Discard</source>
+        <translation>إهمال</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>مساعدة</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>تطبيق</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>إعادة الضبط</translation>
+    </message>
+    <message>
+        <source>Restore Defaults</source>
+        <translation>استعادة الافتراضي</translation>
+    </message>
+</context>
+<context>
+    <name>QWidgetTextControl</name>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>&amp;تراجع</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>إ&amp;عادة</translation>
+    </message>
+    <message>
+        <source>Cu&amp;t</source>
+        <translation>قص&amp;</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;نسخ</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Link Location</source>
+        <translation>نسخ &amp;مكان الرابط</translation>
+    </message>
+    <message>
+        <source>&amp;Paste</source>
+        <translation>&amp;لصق</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>حذف</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation>تحديد الكل</translation>
+    </message>
+</context>
+</TS>
