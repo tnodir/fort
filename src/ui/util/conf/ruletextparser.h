a28cd6cfcfd3399163f353af5fc12d3b640a6759
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2024-05-18 12:11:09 +0300

    UI: RuleTextParser: Prepare RuleExpr

--- src/ui/util/conf/ruletextparser.h
+++ src/ui/util/conf/ruletextparser.h
@@ -2,8 +2,33 @@
 #define RULETEXTPARSER_H
 
 #include <QObject>
+#include <QVector>
 
 #include <util/util_types.h>
+
+struct RuleExpr
+{
+    bool cond_or : 1 = false;
+    bool cond_not : 1 = false;
+
+    enum ExprType : qint8 {
+        ExprNone = 0,
+        ExprList,
+        ExprAddress,
+        ExprLocalAddress,
+        ExprPort,
+        ExprLocalPort,
+        ExprDirection,
+        ExprProtocol,
+    };
+
+    ExprType exprType = ExprNone;
+
+    quint16 listIndex = 0;
+    quint16 listCount = 0;
+
+    StringViewList viewList;
+};
 
 class RuleTextParser : public QObject
 {
@@ -12,7 +37,9 @@ class RuleTextParser : public QObject
 public:
     explicit RuleTextParser(const QString &text, QObject *parent = nullptr);
 
-    const StringViewList &viewList() const { return m_viewList; }
+    RuleExpr::ExprType exprType() const { return m_exprType; }
+
+    const QVector<RuleExpr> &ruleExprList() const { return m_ruleExprList; }
 
     bool parse();
 
@@ -24,10 +51,12 @@ private:
 private:
     bool m_skipLine : 1 = false;
 
+    RuleExpr::ExprType m_exprType = RuleExpr::ExprNone;
+
     const QChar *m_p = nullptr;
     const QChar *m_end = nullptr;
 
-    StringViewList m_viewList;
+    QVector<RuleExpr> m_ruleExprList;
 };
 
 #endif // RULETEXTPARSER_H
