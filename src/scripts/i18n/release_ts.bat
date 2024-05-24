08a176f7325bbe5d83374b4e0e6d30e541c2602f
BeckyDTP <becky@fr.pl>
2024-05-24 19:59:34 +0200

    pl in release_ts

--- src/scripts/i18n/release_ts.bat
+++ src/scripts/i18n/release_ts.bat
@@ -3,6 +3,6 @@
 
 cd ..\..\ui
 
-for %%L in (ar,de,fr,it,ko,pt_BR,ru,sl,zh_CN) do (
+for %%L in (ar,de,fr,it,ko,pt_BR,ru,sl,zh_CN,pl) do (
 	%QT_HOME%\bin\lrelease -removeidentical i18n\i18n_%%L.ts i18n\qt\qtbase_%%L.ts -qm i18n\i18n_%%L.qm
 )
