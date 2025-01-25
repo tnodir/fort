4e649e0057aba058a90fa0f9d0df96c913ea66cd
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2025-01-25 17:18:54 +0500

    UI: Update translations

--- src/ui/i18n/i18n_ko.ts
+++ src/ui/i18n/i18n_ko.ts
@@ -736,7 +736,7 @@
     </message>
     <message>
         <source>Integration</source>
-        <translation type="unfinished"></translation>
+        <translation>통합</translation>
     </message>
     <message>
         <source>Windows Explorer integration</source>
@@ -744,7 +744,7 @@
     </message>
     <message>
         <source>Control Panel: Sounds integration</source>
-        <translation type="unfinished"></translation>
+        <translation>제어판: 소리 통합</translation>
     </message>
     <message>
         <source>Auto-run:</source>
