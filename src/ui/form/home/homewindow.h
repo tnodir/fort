0f2c6fa74450f12874f49bda501771f1c48aacc6
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2025-01-29 15:17:12 +0500

    UI: MyFort: Remove "Statistics" button

--- src/ui/form/home/homewindow.h
+++ src/ui/form/home/homewindow.h
@@ -55,7 +55,6 @@ private:
     QToolButton *m_btProfile = nullptr;
     QToolButton *m_btLogs = nullptr;
     QToolButton *m_btServiceLogs = nullptr;
-    QToolButton *m_btStat = nullptr;
     QToolButton *m_btReleases = nullptr;
     QToolButton *m_btHelp = nullptr;
 };
