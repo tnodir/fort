f3e542e9f59e90060b7090d80ed16c6eaa39609f
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2025-02-05 16:56:58 +0500

    UI: StatConnManager: Minor refactor

--- src/ui/stat/statconnmanager.cpp
+++ src/ui/stat/statconnmanager.cpp
@@ -25,12 +25,15 @@ bool migrateFunc(SqliteDb *db, int version, bool isNewDb, void *ctx)
 {
     Q_UNUSED(db);
     Q_UNUSED(version);
+    Q_UNUSED(isNewDb);
     Q_UNUSED(ctx);
 
+#if 0
     if (isNewDb) {
         // COMPAT: DB schema
         return true;
     }
+#endif
 
     return true;
 }
