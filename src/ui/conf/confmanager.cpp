6de2bff0baa8740f1cbf6067fb58d76c8fa20913
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2025-02-23 13:57:16 +0500

    UI: ConfManager: Bump DB version

--- src/ui/conf/confmanager.cpp
+++ src/ui/conf/confmanager.cpp
@@ -30,7 +30,7 @@ namespace {
 
 const QLoggingCategory LC("conf");
 
-constexpr int DATABASE_USER_VERSION = 49;
+constexpr int DATABASE_USER_VERSION = 50;
 
 constexpr int CONF_PERIODS_UPDATE_INTERVAL = 60 * 1000; // 1 minute
 
