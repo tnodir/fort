dd5343d4915a3473fd056980ac63f48e76989404
coolvitto <160416811+coolvitto@users.noreply.github.com>
2025-03-19 10:30:36 +0900

    Update i18n_ja.ts

--- src/ui/i18n/i18n_ja.ts
+++ src/ui/i18n/i18n_ja.ts
@@ -1337,7 +1337,7 @@ Make sure that you have a fresh backup.</source>
     </message>
     <message>
         <source>Trace Driver Events</source>
-        <translation type="unfinished"></translation>
+        <translation>ドライバーイベントのトレース</translation>
     </message>
 </context>
 <context>
