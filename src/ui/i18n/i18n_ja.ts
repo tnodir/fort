b1d064d4f22183a77b542d75d4716721241ff316
coolvitto <160416811+coolvitto@users.noreply.github.com>
2025-04-29 09:59:13 +0900

    Update i18n_ja.ts 2025-04-29

--- src/ui/i18n/i18n_ja.ts
+++ src/ui/i18n/i18n_ja.ts
@@ -1058,11 +1058,11 @@
     </message>
     <message>
         <source>Auto-Show Window</source>
-        <translation type="unfinished"></translation>
+        <translation>ウィンドウの自動表示</translation>
     </message>
     <message>
         <source>Auto-Show Window, when Tray Icon is hidden</source>
-        <translation type="unfinished"></translation>
+        <translation>ウィンドウの自動表示 (トレイアイコンが非表示のとき)</translation>
     </message>
 </context>
 <context>
